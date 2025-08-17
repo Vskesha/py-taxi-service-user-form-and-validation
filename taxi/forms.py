7d94dfd51bcc6f4f7e5eabf3e4e96ba2cb3ef8a7
Vasyl Boliukh <vskesha@gmail.com>
2025-08-17 23:17:48 +0300

    fixed getting user model

--- taxi/forms.py
+++ taxi/forms.py
@@ -2,7 +2,9 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
-from taxi.models import Driver, Car
+from taxi.models import Car
+
+Driver = get_user_model()
 
 
 class DriverCreationForm(UserCreationForm):
--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
@@ -5,7 +6,9 @@ from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from taxi.forms import DriverCreationForm, DriverLicenseUpdateForm, CarForm
-from taxi.models import Driver, Car, Manufacturer
+from taxi.models import Car, Manufacturer
+
+Driver = get_user_model()
 
 
 @login_required
