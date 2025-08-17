2801a901c92d7db56f8591f51a95dd9bfcdfaf94
Vasyl Boliukh <vskesha@gmail.com>
2025-08-17 15:50:50 +0300

    implemented create and delete views for drivers

--- taxi/urls.py
+++ taxi/urls.py
@@ -7,6 +7,8 @@ from .views import (
     CarCreateView,
     CarUpdateView,
     CarDeleteView,
+    DriverCreateView,
+    DriverDeleteView,
     DriverListView,
     DriverDetailView,
     ManufacturerListView,
@@ -43,9 +45,9 @@ urlpatterns = [
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
-    ),
+    path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
+    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
+    path("drivers/<int:pk>/delete/", DriverDeleteView.as_view(), name="driver-delete"),
 ]
 
 app_name = "taxi"
